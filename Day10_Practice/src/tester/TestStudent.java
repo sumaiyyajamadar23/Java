59cfc2877b7c143b4de4038e64a03bad2eb24a4d
dac <dac@dypm.org>
2023-05-08 20:03:29 +0530

    1-7 Que

--- Day10_Practice/src/tester/TestStudent.java
+++ Day10_Practice/src/tester/TestStudent.java
@@ -16,26 +16,42 @@ public class TestStudent {
 	public static void main(String[] args) {
 		try(Scanner sc=new Scanner(System.in))
 		{
+			//1. Display all student details from a student list
+
 		List<Student> lists=populateList();
 		System.out.println("Students: ");
 		lists.forEach(s->System.out.println(s));
-		//Display all student details from a student map
+		//2.Display all student details from a student map
 		System.out.println("MAPPPPPPPP!!!!!!!!!!!!!");
        Map<String, Student> map1=populateMap(lists);
        map1.values().forEach(m ->System.out.println(m));
-       // Display all student details(list) sorted as per GPA  Use custom orderin
+       //3.Display all student details(list) sorted as per GPA  Use custom orderin
        System.out.println("Sortinggg as per gpaaaaaaaaaaaa");
        lists.stream()
         .sorted((p1, p2) -> ((Double)p1.getGpa()).compareTo(p2.getGpa()))
     	 .forEach(s->System.out.println(s)); 
-      //	Write a tester to print average of  gpa of students (map)for the specified subject eg : i/p : subject	   
+      //4.Write a tester to print average of  gpa of students (map)for the specified subject eg : i/p : subject	   
 	Subject sub=Subject.valueOf(sc.next());
-       map1.values()
+     OptionalDouble std= map1.values()
 	.stream()
-	.filter(s1->s1.getSubject()==sub)
-	.mapToDouble(s1->s1.getGpa())
-	.average()
-	.forEach(s1->System.out.println(s1)); 
+	.filter(s1->s1.getSubject().equals(sub))
+	.mapToDouble(s1->s1.getGpa()) 
+	.average();
+      if(std.isPresent())
+	   System.out.println(std);
+      else
+	  System.out.println("Wrongggg!!!")
+  //5 Print name of specified subject  topper
+  //lists.stream()
+     Optional<Student> std1= lists.stream()
+      .filter(s->s.getSubject().equals(sub))
+      .max((p1, p2) -> ((Double) p1.getGpa()).compareTo(p2.getGpa()));
+     // System.out.println(std1.get().getName());
+  if (std1.isPresent())
+	  System.out.println( std1.get().getName()+" Is topper of Subject:"+std1.get().getSubject());
+	  else
+	System.out.println("wrongggg!!!!!");
+
 		}
        
        
