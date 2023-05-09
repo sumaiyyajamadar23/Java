eb1f98aef8abadff58bc1e169215803dd78168a2
dac <dac@dypm.org>
2023-05-09 20:13:18 +0530

    D11 Employee(using serialization,de-serialization)

--- EmpManagement_11/src/Tester/CustomTester.java
+++ EmpManagement_11/src/Tester/CustomTester.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import static utils.IoUtils.*;
+
+import com.app.core.Dept;
 import com.app.core.Emp;
 import static utils.EmpUtils.*;
 import static utils.ValidationRules.*;
@@ -21,7 +23,13 @@ public class CustomTester {
 		Map<String, Emp> custMapMap=restore(file);
 		boolean exit=false;
 		while(!exit)
-		{
+		{System.out.println("1. Hire new emp"
+				+ "2. List all emp details"
+				+ "3. Promote an emp"
+				+ "4. Delete emp details"
+				+ "5. Sort emps as per join date n display the same."
+				+ "6. We can add many more options here..."
+				+ "0. Exit");
 			try
 			{
 				switch(sc.nextInt())
@@ -30,6 +38,27 @@ public class CustomTester {
 					exit=true;
 					StoreCustomDetail(file, custMapMap);
 					break;
+				case 1:
+					//1. Hire new emp
+					System.out.println("Enter Details:id,firstName,lastName,dept, joiningDate");
+				Emp newEmp=ValidateAll(sc.next(), sc.next(), sc.next(), sc.next(),sc.next(), custMapMap);
+					custMapMap.put(newEmp.getId(), newEmp);
+					System.out.println("New Employee:"+newEmp.getFirstName()+"Is hired");
+					break;
+				case 2:
+					System.out.println("All Employee Etails:");
+					
+					//2. List all emp details
+					break;
+				case 3:
+					//3. Promote an emp
+					break;
+				case 4:
+					//4. Delete emp details
+				case 5:
+					//5. Sort emps as per join date n display the same.
+					break;
+					
 				}
 			}
 			catch(Exception e)
